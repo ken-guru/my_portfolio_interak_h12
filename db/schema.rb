b65b349ec967ab5eb6351cad11f91ec749948801
Ken Paulsen <ken.paulsen@c-k.no>
2014-01-21 15:16:56 +0100

    Added basic model for Projects
    Now starting implementation of Controller and Views

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117123019) do
+ActiveRecord::Schema.define(version: 20140121135440) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
     t.string   "website"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "url"
+    t.date     "delivered_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
