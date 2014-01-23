1b2733561c8bf02785571215b77aa8198e765443
Ken Paulsen <ken.paulsen@c-k.no>
2014-01-23 17:00:41 +0100

    Completed basic Projects functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,23 +1,50 @@
 class ProjectsController < ApplicationController
+
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
+    @projects = Project.all
   end
 
-  def show
-  end
+  def show;end
 
   def new
     @project = Project.new
   end
 
   def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project
+    else
+      render action: 'new'
+    end
   end
 
-  def edit
-  end
+  def edit;end
 
   def update
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @project.destroy
+
+    redirect_to projects_path
   end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:name, :description, :delivered_at, :url)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
 end
